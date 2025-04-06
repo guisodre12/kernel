405dcc1ac6bd8f7848808298fd6b1d4d98f9fdca
Guilherme Sodré <92101150+GuiSodre12@users.noreply.github.com>
2025-04-06 16:00:31 -0300

    Add files via upload

---
+++ include/gpio.h
@@ -0,0 +1,8 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+void gpio_output(char port, unsigned int pin);
+void gpio_write(char port, unsigned int, value);
+void gpio_toggle(char port, unsigned int, pin);
+
+#endif
---
+++ src/gpio.c
@@ -0,0 +1,34 @@
+#include <avr/io.h>
+#include "gpio.h"
+
+void gpio_output(char port, unsigned int pin) {
+  if(port == 'B') DDRB |= (1 << pin);
+  else if(port == 'C') DDRC |= (1 << pin);
+  else if(port == 'D') DDRD |= (1 << pin);
+}
+
+void gpio_write(char port, unsigned int value){
+  switch(port){
+    case 'B':
+     if(value) PORTB |= (1 << pin);
+     else PORTB &= ~(1 << pin);
+    
+    case 'C':
+     if(value) PORTB |= (1 << pin);
+     else PORTB &= ~(1 << pin);
+    
+    case 'D':
+     if(value) PORTB |= (1 << pin);
+     else PORTB &= ~(1 << pin);
+  }
+}
+
+void gpio_toggle(char port, unsigned int pin){
+  switch(port){
+    case 'B':
+     PORTB ^= (1 << pin); break;
+    case 'C':
+     PORTC ^= (1 << pin); break;
+    case 'D':
+     PORTD ^= (1 << pin); break;
+}
