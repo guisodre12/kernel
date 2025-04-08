c841d03d036f485de6f538709a944c4b06e70d69
Guilherme Sodré <92101150+GuiSodre12@users.noreply.github.com>
2025-04-08 01:03:13 -0300

    Update gpio.h

--- include/gpio.h
+++ include/gpio.h
@@ -2,7 +2,7 @@
 #define GPIO_H
 
 void gpio_output(char port, unsigned int pin);
-void gpio_write(char port, unsigned int, value);
-void gpio_toggle(char port, unsigned int, pin);
+void gpio_write(char port, unsigned int pin, unsigned int value);
+void gpio_toggle(char port, unsigned int pin);
 
 #endif
