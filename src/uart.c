30df96152b8830f631e73aa571ddddc6bc689293
Guilherme Sodré <92101150+guisodre12@users.noreply.github.com>
2025-10-22 03:54:36 +0000

    Atualizar o uart.c

--- src/uart.c
+++ src/uart.c
@@ -6,19 +6,17 @@
 #define UBRR_VALUE ((F_CPU/16/BAUD)-1)
 
 void uart_init(void) {
-    // Set baud rate
+    //  baud rate
     UBRR0H = (UBRR_VALUE >> 8);
     UBRR0L = UBRR_VALUE;
 
-    // Enable transmitter and receiver
+    // transmitter and receiver
     UCSR0B = (1 << TXEN0) | (1 << RXEN0);
 
-    // Set frame format: 8 data bits, 1 stop bit
     UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 }
 
 void uart_send_char(char c) {
-    // Wait until data register is empty
     while (!(UCSR0A & (1 << UDRE0)));
     UDR0 = c;
 }
@@ -30,7 +28,7 @@ void uart_send_string(const char* str) {
 }
 
 char uart_receive_char(void) {
-    // Wait until data is received
+    // receber
     while (!(UCSR0A & (1 << RXC0)));
     return UDR0;
 }
